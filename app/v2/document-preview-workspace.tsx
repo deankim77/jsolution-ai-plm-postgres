da44144e45fc12452ce1c52b58c572240ec69ca7
deankim77 <dean@jjjsolution.com>
2026-09-02 23:45:54 +0900

    Restore contract-aligned V2 document wrapper

--- app/v2/document-preview-workspace.tsx
+++ app/v2/document-preview-workspace.tsx
@@ -6,10 +6,10 @@ import DocumentManagementPanel from "./document-management-panel";
 const Impl=lazy(()=>import("./document-preview-workspace-impl").then(module=>({default:module.PreviewDocumentsWorkspace})));
 type LibraryMode="document"|"drawing";
 
-function DocumentWorkspaceBridge(props:any){
+function DocumentWorkspace(props:any){
+  const [libraryMode,setLibraryMode]=useState<LibraryMode>("document");
   const [detailId,setDetailId]=useState("");
   const [refreshKey,setRefreshKey]=useState(0);
-  const [libraryMode,setLibraryMode]=useState<LibraryMode>("document");
 
   useEffect(()=>{
     const refresh=()=>setRefreshKey(value=>value+1);
@@ -18,24 +18,9 @@ function DocumentWorkspaceBridge(props:any){
   },[]);
 
   return <>
-    <Suspense fallback={null}>
-      <Impl
-        key={refreshKey}
-        {...props}
-        libraryMode={libraryMode}
-        onLibraryModeChange={setLibraryMode}
-        onOpenDetail={(deliverableId:string)=>setDetailId(deliverableId)}
-      />
-    </Suspense>
-    {detailId&&<DocumentManagementPanel
-      deliverableId={detailId}
-      onClose={()=>setDetailId("")}
-      onOpenTask={(projectId,taskId)=>{
-        setDetailId("");
-        props.onOpenTask?.(projectId,taskId);
-      }}
-    />}
+    <Suspense fallback={null}><Impl key={refreshKey} {...props} libraryMode={libraryMode} onLibraryModeChange={setLibraryMode} onOpenDetail={setDetailId}/></Suspense>
+    {detailId&&<DocumentManagementPanel deliverableId={detailId} onClose={()=>setDetailId("")} onOpenTask={(projectId,taskId)=>{setDetailId("");props.onOpenTask?.(projectId,taskId)}}/>}
   </>;
 }
 
-export const PreviewDocumentsWorkspace=((props:any)=><DocumentWorkspaceBridge {...props}/>) as ComponentType<any>;
+export const PreviewDocumentsWorkspace=((props:any)=><DocumentWorkspace {...props}/>) as ComponentType<any>;
